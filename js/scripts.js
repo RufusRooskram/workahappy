52605e92cf72a6266fd4b7a4b600a309ddac6fd8
Sealtiel Rosas <43919348+SealtielRosas@users.noreply.github.com>
2020-06-07 00:15:45 -0500

    sfoihg

--- js/scripts.js
+++ js/scripts.js
@@ -34,9 +34,10 @@ window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
+var banner = document.getElementById("banner");
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= banner) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
