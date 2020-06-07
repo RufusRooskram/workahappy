6d7540d68ca790da4c9f6d81f4de4ac0ac746f06
Sealtiel Rosas <43919348+SealtielRosas@users.noreply.github.com>
2020-06-06 23:58:05 -0500

    adfs

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,15 @@ $('.owl-calen').owlCarousel({
         }
     }
 })
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
