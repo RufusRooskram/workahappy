77764dc96f30705e7527aedab6186870a41b86d0
rodrigoAktien <61477816+rodrigoAktien@users.noreply.github.com>
2020-06-16 18:07:17 -0500

    cambios

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,10 @@ $('.owl-calen').owlCarousel({
             items:1
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
-            items:3
+            items:1
         }
     }
 })
